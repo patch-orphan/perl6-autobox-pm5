a1d878672c0664be65c7db5e2bf35e762d39e614
Nick Patch <n@atemoya.net>
2010-04-21 21:50:19 -0400

    initial import

---
+++ lib/Perl6/Autobox.pm
@@ -0,0 +1,44 @@
+package Perl6::Autobox;
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Perl6::Autobox - XXX
+
+=head1 VERSION
+
+This document describes Perl6::Autobox version 0.01.
+
+=head1 SYNOPSIS
+
+    use Perl6::Autobox;
+
+=head1 DESCRIPTION
+
+XXX
+
+=head1 SEE ALSO
+
+XXX
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
